99f0f294f1c67e8ce168b88da3811783e46e1bc0
Geiser Menoia <gmenoiaa@gmail.com>
2021-07-29 22:31:27 +0200

    add image and fix partner position

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
+import {useEffect, useState} from 'react'
 import CytoscapeComponent from 'react-cytoscapejs/src/component'
 import cytoscape from 'cytoscape'
+import nodeHtmlLabel from 'cytoscape-node-html-label'
 import dagre from 'cytoscape-dagre'
 import {buildData} from './data'
-import {useEffect, useState} from 'react'
+import './App.css';
 
 cytoscape.use(dagre)
 
+if (typeof cytoscape('core', 'nodeHtmlLabel') === 'undefined') {
+  nodeHtmlLabel(cytoscape);
+}
+
 const data = buildData(3)
-const persons = Object.entries(data.persons).map(([id, person]) => ({ data: { id: id, name: person.name, gender: person.gender, type: 'person' } }))
+const persons = Object.entries(data.persons).map(([id, person]) => ({ data: { id: id, name: person.name, gender: person.gender, type: 'person' }, classes: 'l1' }))
 const unions = Object.keys(data.unions).map(id => ({ data: { id, type: 'union' } }))
 const edges = data.links.map(([left, right]) => ({ data: { id: `${left}-${right}`, source: left, target: right } }))
 
@@ -26,14 +31,19 @@ function App() {
 
       for (const union of unions) {
         // make the union point at the same y position as the nodes
-        const firstIncomingNode = union.incomers().find(element => element.isNode())
-        union.position('y', firstIncomingNode.position('y'))
+        const incomingNodes = union.incomers().filter(element => element.isNode())
+        union.position('y', incomingNodes[0].position('y'))
 
-        // const outgoingEdges = union.outgoers().filter(element => element.isEdge())
-        //
-        // for (const edge of outgoingEdges) {
-        //   console.log(edge.sourceEndpoint())
-        // }
+        // adjust how far partners are from union node
+        const unionX = union.position('x')
+        for (const incomingNode of incomingNodes) {
+          const nodeX = incomingNode.position('x')
+          if (Math.abs(nodeX - unionX) > 200) {
+            console.log(incomingNode.data(), nodeX, unionX)
+            incomingNode.position('x', unionX + (nodeX > unionX ? 100 : -100))
+          }
+        }
+
       }
 
     }
@@ -43,24 +53,13 @@ function App() {
     {
       selector: 'node[type = "person"]',
       style: {
-        'content': 'data(name)',
-        'text-valign': 'center',
-        'text-halign': 'center',
+        // 'content': 'data(name)',
+        // 'text-valign': 'center',
+        // 'text-halign': 'center',
         'shape': 'rectangle',
         'width': 150,
         'height': 50,
-      }
-    },
-    {
-      selector: 'node[gender = "male"]',
-      style: {
-        'background-color': '#ade1ff',
-      }
-    },
-    {
-      selector: 'node[gender = "female"]',
-      style: {
-        'background-color': '#f3c2c2',
+        'background-color': '#fff'
       }
     },
     {
@@ -93,10 +92,33 @@ function App() {
     edges
   })
 
+  // const elements = CytoscapeComponent.normalizeElements(oldData)
+
   useEffect(() => {
     if (!cy) {
       return
     }
+
+    cy.nodeHtmlLabel([
+      {
+        query: '.l1',
+        halign: "center",
+        valign: "center",
+        halignBox: "center",
+        valignBox: "center",
+        tpl: (data) => {
+          return `<div class="card ${data.gender}">
+                <div class="card__image">
+                    <img alt="Card image" src="https://previews.123rf.com/images/zurijeta/zurijeta1107/zurijeta110700136/10087069-closeup-profile-on-a-good-looking-old-man.jpg" />
+                </div>
+                <div class="card__details">
+                    <span class="card__details__name">${data.name}</span>
+                    <span class="card__details__birth">01.01.1990</span>
+                </div>
+            </div>`
+        }
+      }
+    ]);
 
     cy.fit()
   }, [cy])
--- src/data.js
+++ src/data.js
@@ -151,48 +151,48 @@ export function buildData(maxLevels = 2) {
     }
 }
 
-//
-// {
-//     nodes: [
-//         { data: { id: 'id1', name: 'Francis', type: 'person', gender: 'male' } },
-//         { data: { id: 'id2', name: 'Greta', type: 'person', gender: 'female' } },
-//         { data: { id: 'id3', name: 'Eric', type: 'person', gender: 'male' } },
-//         { data: { id: 'id4', name: 'Charlene', type: 'person', gender: 'female' } },
-//         { data: { id: 'id5', name: 'Iver', type: 'person', gender: 'male' } },
-//         { data: { id: 'id6', name: 'Dan', type: 'person', gender: 'male' } },
-//         { data: { id: 'id7', name: 'Klaus', type: 'person', gender: 'male' } },
-//         { data: { id: 'id8', name: 'Heinz', type: 'person', gender: 'male' } },
-//         { data: { id: 'id9', name: 'Jennifer', type: 'person', gender: 'female' } },
-//         { data: { id: 'id10', name: 'Lennart', type: 'person', gender: 'male' } },
-//         { data: { id: 'id11', name: 'Adam', type: 'person', gender: 'male' } },
-//         { data: { id: 'id12', name: 'Berta', type: 'person', gender: 'female' } },
-//
-//         { data: { id: 'u1', type: 'union' } },
-//         { data: { id: 'u2', type: 'union' } },
-//         { data: { id: 'u3', type: 'union' } },
-//         { data: { id: 'u4', type: 'union' } },
-//         { data: { id: 'u5', type: 'union' } },
-//     ],
-//       edges: [
-//     { data: { id: 'id1-u1', source: 'id1', target: 'u1' } },
-//     { data: { id: 'id2-u1', source: 'id2', target: 'u1' } },
-//     { data: { id: 'u1-id3', source: 'u1', target: 'id3' } },
-//
-//     { data: { id: 'id3-u2', source: 'id3', target: 'u2' } },
-//     { data: { id: 'id4-u2', source: 'id4', target: 'u2' } },
-//     { data: { id: 'u2-id8', source: 'u2', target: 'id8' } },
-//
-//     { data: { id: 'id8-u3', source: 'id8', target: 'u3' } },
-//     { data: { id: 'u3-id10', source: 'u3', target: 'id10' } },
-//
-//     { data: { id: 'id4-u4', source: 'id4', target: 'u4' } },
-//     { data: { id: 'id5-u4', source: 'id5', target: 'u4' } },
-//     { data: { id: 'u4-id9', source: 'u4', target: 'id9' } },
-//
-//     { data: { id: 'id11-u5', source: 'id11', target: 'u5' } },
-//     { data: { id: 'id12-u5', source: 'id12', target: 'u5' } },
-//     { data: { id: 'u5-id4', source: 'u5', target: 'id4' } },
-//     { data: { id: 'u5-id6', source: 'u5', target: 'id6' } },
-//     { data: { id: 'u5-id7', source: 'u5', target: 'id7' } },
-// ]
-// }
+
+export const oldData = {
+    nodes: [
+        { data: { id: 'id1', name: 'Francis', type: 'person', gender: 'male' } },
+        { data: { id: 'id2', name: 'Greta', type: 'person', gender: 'female' } },
+        { data: { id: 'id3', name: 'Eric', type: 'person', gender: 'male' } },
+        { data: { id: 'id4', name: 'Charlene', type: 'person', gender: 'female' } },
+        { data: { id: 'id5', name: 'Iver', type: 'person', gender: 'male' } },
+        { data: { id: 'id6', name: 'Dan', type: 'person', gender: 'male' } },
+        { data: { id: 'id7', name: 'Klaus', type: 'person', gender: 'male' } },
+        { data: { id: 'id8', name: 'Heinz', type: 'person', gender: 'male' } },
+        { data: { id: 'id9', name: 'Jennifer', type: 'person', gender: 'female' } },
+        { data: { id: 'id10', name: 'Lennart', type: 'person', gender: 'male' } },
+        { data: { id: 'id11', name: 'Adam', type: 'person', gender: 'male' } },
+        { data: { id: 'id12', name: 'Berta', type: 'person', gender: 'female' } },
+
+        { data: { id: 'u1', type: 'union' } },
+        { data: { id: 'u2', type: 'union' } },
+        { data: { id: 'u3', type: 'union' } },
+        { data: { id: 'u4', type: 'union' } },
+        { data: { id: 'u5', type: 'union' } },
+    ],
+      edges: [
+    { data: { id: 'id1-u1', source: 'id1', target: 'u1' } },
+    { data: { id: 'id2-u1', source: 'id2', target: 'u1' } },
+    { data: { id: 'u1-id3', source: 'u1', target: 'id3' } },
+
+    { data: { id: 'id3-u2', source: 'id3', target: 'u2' } },
+    { data: { id: 'id4-u2', source: 'id4', target: 'u2' } },
+    { data: { id: 'u2-id8', source: 'u2', target: 'id8' } },
+
+    { data: { id: 'id8-u3', source: 'id8', target: 'u3' } },
+    { data: { id: 'u3-id10', source: 'u3', target: 'id10' } },
+
+    { data: { id: 'id4-u4', source: 'id4', target: 'u4' } },
+    { data: { id: 'id5-u4', source: 'id5', target: 'u4' } },
+    { data: { id: 'u4-id9', source: 'u4', target: 'id9' } },
+
+    { data: { id: 'id11-u5', source: 'id11', target: 'u5' } },
+    { data: { id: 'id12-u5', source: 'id12', target: 'u5' } },
+    { data: { id: 'u5-id4', source: 'u5', target: 'id4' } },
+    { data: { id: 'u5-id6', source: 'u5', target: 'id6' } },
+    { data: { id: 'u5-id7', source: 'u5', target: 'id7' } },
+]
+}
